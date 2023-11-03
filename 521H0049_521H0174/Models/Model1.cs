d4ee19d981c27b85a0e44c6a961b84d82ba10e9a
Suimei1208 <103234140+Suimei1208@users.noreply.github.com>
2023-11-03 16:15:27 +0700

    Add project files.

---
+++ 521H0049_521H0174/Models/Model1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace _521H0049_521H0174.Models
+{
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=StudentsInformation")
+        {
+        }
+
+        public virtual DbSet<Certificate> Certificates { get; set; }
+        public virtual DbSet<LoginHistory> LoginHistories { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
